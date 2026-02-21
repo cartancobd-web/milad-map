247e19b212b8a306a962e294e15aa0a20f9a12ff
cartancobd-web <cartancobd@gmail.com>
2026-02-22 00:54:17 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,9 +2,13 @@ const firebaseConfig = {
   apiKey: "AIzaSyAETli2UtjPzEU5xd9U6_W7xeJxBGq3xV8",
   authDomain: "milad-4727b.firebaseapp.com",
   projectId: "milad-4727b",
+  storageBucket: "milad-4727b.firebasestorage.app",
+  messagingSenderId: "1088600725533",
+  appId: "1:1088600725533:web:f4bdacb68606f846590ef4",
 };
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 
 const db = firebase.firestore();
+
